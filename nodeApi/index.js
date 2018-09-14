8cadbf4692834d3f294348f5b808e076ae157ce5
3lv27 <elvin.gm27@gmail.com>
2018-09-14 19:23:10 +0200

    Parsing Request Paths

--- nodeApi/index.js
+++ nodeApi/index.js
@@ -1,11 +1,27 @@
 
 // Dependencies
 const http = require('http')
+const url = require('url')
 
 
 //Init Server
 const server = http.createServer(function (req, res) {
-  res.end('Hello World!\n')
+
+	//Get the URL and parse it
+	const parsedUrl = url.parse(req.url, true)
+
+	//Get the path
+	const path = parsedUrl.pathname
+	const trimmedPath = path.replace(/^\/+|\/+$/g,'')
+
+	//Send the response
+	res.end('Hello World!\n')
+
+	//Log the request path
+	console.log('Request received on this path: ', trimmedPath)
+
+
+  
 })
 
 //Start the server listening on port 3000
