b2c47f4156d8112c62b801bf4159ab3d24a78831
3lv27 <elvin.gm27@gmail.com>
2018-09-15 12:15:30 +0200

    parsing http methods

--- nodeApi/index.js
+++ nodeApi/index.js
@@ -5,7 +5,7 @@ const url = require('url')
 
 
 //Init Server
-const server = http.createServer(function (req, res) {
+const server = http.createServer((req, res) => {
 
 	//Get the URL and parse it
 	const parsedUrl = url.parse(req.url, true)
@@ -14,17 +14,20 @@ const server = http.createServer(function (req, res) {
 	const path = parsedUrl.pathname
 	const trimmedPath = path.replace(/^\/+|\/+$/g,'')
 
+	//Get the HTTP method
+	const method = req.method.toLowerCase()
+
 	//Send the response
 	res.end('Hello World!\n')
 
 	//Log the request path
-	console.log('Request received on this path: ', trimmedPath)
+	console.log('Request received on this path: [', trimmedPath, '] with method: [',method,']')
 
 
   
 })
 
 //Start the server listening on port 3000
-server.listen(3000, function () {
+server.listen(3000, () => {
   console.log('Server listening on port 3000')
 })
